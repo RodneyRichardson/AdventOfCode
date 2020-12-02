aa76952616a5925b08b1efe2e3c669ff66eb44b3
Rodney Richardson <rodney.richardson@cambridgeconsultants.com>
2020-12-02 12:22:32 +0000

    Day1

---
+++ Day1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode2020
+{
+    public class Day1
+    {
+        public async Task<Int64> Process(String[] input)
+        {
+            int result = 0;
+
+            List<Int64> sorted = new List<Int64>(input.Select(s => Int64.Parse(s)));
+            sorted.Sort();
+
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                for (int j = sorted.Count - 1; j > i; j--)
+                {
+                    for (int k = j - 1; k > i; k--)
+                    {
+                        if (sorted[i] + sorted[j] + sorted[k] == 2020)
+                        {
+                            return sorted[i] * sorted[j] * sorted[k];
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AdventofCode2020
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            String[] input = await File.ReadAllLinesAsync(@"Inputs\InputDay1.txt");
+            Int64 result = await new Day1().Process(input);
+
+        }
+
+    }
+}
