6396c00c2bc0be4063666a5f33380968179d04dc
Rodney Richardson <rodney.richardson@cambridgeconsultants.com>
2022-12-05 12:42:27 +0000

    Day 3

--- 2022/aoc_2022_3.py
+++ 2022/aoc_2022_3.py
@@ -2,15 +2,43 @@
 
 import pathlib
 import sys
+import itertools
+
+def get_priority(item):
+    if item.islower():
+        priority = ord(item) - ord('a') + 1
+    else:
+        priority = ord(item) - ord('A') + 27
+    return priority
 
 def parse(puzzle_input):
     """Parse input."""
+    data = [line.strip() for line in puzzle_input.split('\n')]
+    return data
 
 def part1(data):
     """Solve part 1."""
+    common_items = []
+    for rucksack_items in data:
+        compartment_size = int(len(rucksack_items)/2)
+        compartment1 = set(rucksack_items[0:compartment_size])
+        compartment2 = set(rucksack_items[compartment_size:])
+        common_items.extend(compartment1.intersection(compartment2))
+
+    score = sum(get_priority(item) for item in common_items)
+    return score
+
 
 def part2(data):
     """Solve part 2."""
+    badges = []
+    for i in range(0, len(data), 3):
+        rucksack1 = set(data[i])
+        rucksack2 = set(data[i+1])
+        rucksack3 = set(data[i+2])
+        badges.extend(rucksack1.intersection(rucksack2).intersection(rucksack3))
+    score = sum(get_priority(badge) for badge in badges)
+    return score
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
@@ -31,6 +59,6 @@ if __name__ == "__main__":
 
     for path in paths:
         print(f"{path}:")
-        puzzle_input = pathlib.Path(path).open().readlines()
+        puzzle_input = pathlib.Path(path).read_text().strip()
         solutions = solve(puzzle_input)
         print("\n".join(str(solution) for solution in solutions))
